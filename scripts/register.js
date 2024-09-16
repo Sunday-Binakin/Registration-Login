7dd70dcc8d80b31b7247531f3f0b91939aa4a2c4
SBinakin <sundaybinakin@gmail.com>
2024-09-16 11:33:58 +0000

    intial commit

---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+// alert("javascript works!!!!!");
+//select form by id
+const registerForm = document.getElementById("register-form");
+//handle form submit event
+registerForm.onsubmit=()=>{
+    //collect username
+    //collect email
+    //collect password
+    //save user information
+    //send confirmation email
+};
